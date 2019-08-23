ada74bee11f7faca32fc1558132e809f12bbfb9f
Lucas Russell Kern <lucaskern8@gmail.com>
2019-08-23 08:22:11 -0400

    Create soldiers on game start and render at different positions based on its properties

--- main.js
+++ main.js
@@ -7,21 +7,53 @@ let { canvas } = init()
 let rotateSpeed = 40
 let EnemySpeed = 10
 let PUDuration = 10
+let soldierCount = 9
 
 //Match Settings
 let score,time
 let powerups = []
 
 //soldiers array
-let soliders=[]
+let soldiers=[]
+let enemies=[]
 
-let soldier = {
-  health: true,
-  pos: 0,
-  type: 0,
-  attack: function() {
+function Soldier() {
+  let self = this
+  this.health = true
+  this.pos = 0
+  this.type = 0
+  this.attack = function() {
 
   }
+  //console.log(this)
+  this.sprite = Sprite({
+    x: self.pos * 100,        // starting x,y position of the sprite
+    y: 500,
+    color: 'blue',  // fill color of the sprite rectangle
+    width: 20,     // width and height of the sprite rectangle
+    height: 20,
+    dx: 2,          // move the sprite 2px to the right every frame
+    changes: function() {
+      //console.log(self.pos);
+      this.x = self.pos * 100
+      this.dx = 2
+      switch(self.type) {
+        case 'swrd':
+          this.color = "blue"
+          break
+        case 'jav':
+          this.color = "red"
+          break
+        case 'shld':
+          this.color = "green"
+          break
+      }
+    }
+  })
+}
+
+let enemy = {
+
 }
 
 /*
@@ -31,14 +63,33 @@ let soldier = {
 function gameStart() {
  score = 0
  time = 0
+ hp = 20
 
  setInterval(timer, 1000);
+
+ for (let i = 0; i < soldierCount; i++) {
+  soldiers[i] = new Soldier()
+  soldiers[i].health = 1
+  soldiers[i].pos = i
+
+  switch(i) {
+    case 0: case 3: case 6:
+      soldiers[i].type = 'swrd'
+      break;
+    case 1: case 4: case 7:
+      soldiers[i].type = 'jav'
+      break;
+    case 2: case 5: case 8:
+      soldiers[i].type = 'shld'
+      break;
+  }
+}
 
  loop.start();    // start the game
 }
 
 
-let sprite = Sprite({
+let sprite2 = Sprite({
   x: 100,        // starting x,y position of the sprite
   y: 80,
   color: 'red',  // fill color of the sprite rectangle
@@ -49,22 +100,24 @@ let sprite = Sprite({
 
 let loop = GameLoop({  // create the main game loop
   update: function() { // update the game state
-    sprite.update()
-
-    // wrap the sprites position when it reaches
-    // the edge of the screen
-    if (sprite.x > canvas.width) {
-      sprite.x = -sprite.width
+    sprite2.update()
+    for (let i = 0; i < soldiers.length; i++) {
+      soldiers[i].sprite.update()
+      soldiers[i].sprite.changes()
     }
+    
   },
   render: function() { // render the game state
-    sprite.render()
+    sprite2.render()
+    for (let i = 0; i < soldiers.length; i++) {
+      soldiers[i].sprite.render();
+    }
   }
 });
 
 //simple count up timer
 function timer() {
-  console.log("rand");
+  //console.log("rand");
   ++time
 }
 
